6e826aba4634a5e85da938946566484c61319106
sin42 <sin42@DESKTOP-MQ9KKGC>
2022-05-30 15:15:14 +0530

    SampleProject

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,13 @@
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SampleTest 
+{
+	@Test
+	public void testName() throws Exception 
+	{
+		Reporter.log("Heyyy",true);
+		Reporter.log("Heyy-Heyyy",true);
+		Reporter.log("Heyyy-Heyyy-Heyyy",true);
+	}
+}
